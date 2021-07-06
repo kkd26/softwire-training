e519a1bd9badfe53f3f6a95e9605c226922b8461
Krzysztof Druciarek <krzysztof.druciarek@softwire.com>
2021-07-06 10:05:07 +0100

    FizzBuzz task

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+for (var i = 0; i <= 100; i++) {
+    // if (i % 3 == 0) {
+    //     if (i % 5 == 0) console.log("FizzBuzz");
+    //     else console.log("Fizz");
+    // }
+    // else {
+    //     if (i % 5 == 0) console.log("Buzz");
+    //     else console.log(i);
+    // }
+
+    console.log(i % 3 ? (i % 5 ? i : "Buzz") : (i % 5 ? "Fizz" : "FizzBuzz"));
+}
