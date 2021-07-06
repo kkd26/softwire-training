00ed21e6640ae66a955f667d5704a567065d2fa9
Krzysztof Druciarek <krzysztof.druciarek@softwire.com>
2021-07-06 16:44:00 +0100

    Logging for Support Bank

--- SupportBank/Bank.ts
+++ SupportBank/Bank.ts
@@ -2,35 +2,53 @@ import parse from "csv-parse";
 import path from "path";
 import fs from "fs";
 import BankAccount from "./BankAccount";
+import { logger } from "./index";
 
 class Bank {
-  private accounts: { [name: string]: BankAccount };
-
-  constructor() {
-    this.accounts = {};
-  }
+  private accounts: { [name: string]: BankAccount } = {};
+  private transactions: string[][] = [];
 
   fromCSV(file: string): Promise<void> {
+    logger.info("Method call on Bank: fromCSV");
     const _this = this;
-
-    const output: string[][] = [];
 
     return new Promise((res, rej) => {
       fs.createReadStream(path.resolve(file))
         .pipe(parse({ delimiter: "," }))
         .on("data", function (row: string[]) {
-          output.push(row);
+          _this.transactions.push(row);
         })
         .on("end", function () {
-          output.shift(); // Delete column names
-          output.forEach((transaction) => {
-            const [day, month, year] = transaction[0].split("/");
-            const date = new Date(Number(year), Number(month) - 1, Number(day));
-            const from = transaction[1];
-            const to = transaction[2];
-            const narrative = transaction[3];
-            const amount = Number(transaction[4]);
-            _this.transaction(date, from, to, narrative, amount);
+          _this.transactions.shift(); // Delete column names
+          _this.transactions.forEach((transaction, index) => {
+            try {
+              const [day, month, year] = transaction[0].split("/");
+              const date = new Date(
+                Number(year),
+                Number(month) - 1,
+                Number(day)
+              );
+              if (isNaN(date.getTime())) {
+                const error = `Parsing Error. ${file}:${index + 2} - ${
+                  transaction[0]
+                } is not a date in format dd/mm/yyyy`;
+                throw error;
+              }
+              const from = transaction[1];
+              const to = transaction[2];
+              const narrative = transaction[3];
+              const amount = Number(transaction[4]);
+              if (isNaN(amount)) {
+                const error = `Parsing Error. ${file}:${index + 2} - ${
+                  transaction[4]
+                } is not a number`;
+                throw error;
+              }
+              _this.transaction(date, from, to, narrative, amount);
+            } catch (error) {
+              console.error(error);
+              return logger.error(error);
+            }
           });
           res();
         });
@@ -38,6 +56,7 @@ class Bank {
   }
 
   addPerson(name: string) {
+    logger.info(`Method call on Bank: Add Person - ${name}`);
     this.accounts[name] = new BankAccount(name);
     return this.accounts[name];
   }
@@ -54,11 +73,15 @@ class Bank {
     narrative: string,
     amount: number
   ) {
+    logger.info(
+      `Method call on Bank: Transaction - ${date.toDateString()} -- ${from} -- ${to} -- ${narrative} -- ${amount}`
+    );
     this.credit(date, from, narrative, amount);
     this.credit(date, to, narrative, -amount);
   }
 
   listAll() {
+    logger.info(`Method call on Bank: List All`);
     for (const name in this.accounts) {
       if (Object.prototype.hasOwnProperty.call(this.accounts, name)) {
         const account = this.accounts[name];
--- SupportBank/index.ts
+++ SupportBank/index.ts
@@ -1,5 +1,17 @@
 import readline from "readline";
 import Bank from "./Bank";
+import { configure, getLogger } from "log4js";
+
+configure({
+  appenders: {
+    file: { type: "fileSync", filename: "logs/debug.log" },
+  },
+  categories: {
+    default: { appenders: ["file"], level: "error" },
+  },
+});
+
+const logger = getLogger("log.txt");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -16,20 +28,27 @@ function ask(question: string): Promise<string> {
 
 const bank = new Bank();
 
-bank.fromCSV("./Transactions2014.csv").then(async (val) => {
+bank.fromCSV("./DodgyTransactions2015.csv").then(async (val) => {
   while (true) {
     const ans = await ask("\nUsage: List All | List [Name] | quit\n");
+    logger.info("Command: " + ans);
     switch (ans) {
       case "quit":
+        logger.info("Quit");
         rl.close();
+        logger.info("ReadLine stream closed");
         return;
       case "List All":
         bank.listAll();
+        logger.info("List All executed");
         break;
       default:
         if (!ans.startsWith("List ")) continue;
         const name = ans.replace(/^List /, "");
         bank.listTransactions(name);
+        logger.info("List Transactions executed");
     }
   }
 });
+
+export { logger };
